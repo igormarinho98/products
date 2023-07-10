2874f7aeb4e652645e9531e677e92d669735c5de
Igor Marinho <igormarinholeo2014@gmail.com>
2023-07-10 07:30:52 -0300

    investment controller schema updated and investment controller updates

--- src/main/java/webvest/api/controller/InvestmentController.java
+++ src/main/java/webvest/api/controller/InvestmentController.java
@@ -95,7 +95,6 @@ public class InvestmentController {
 	}
 	
 	
-	@CrossOrigin(origins = "http://localhost:8080")
 	@GetMapping("/invest/export")
 	public void exportToCSV(HttpServletResponse response) throws IOException  {
 		try {
